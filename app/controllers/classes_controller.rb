500dbf2f2271eafba5c85be4791ea348883d8321
Daniel Weinmann <danielweinmann@gmail.com>
2015-02-26 23:48:10 -0300

    Adds MEME landing page

--- app/controllers/classes_controller.rb
+++ app/controllers/classes_controller.rb
@@ -1,10 +1,13 @@
 class ClassesController < ApplicationController
   
   after_action :verify_policy_scoped, only: %i[]
-  after_action :verify_authorized, except: %i[index]
+  after_action :verify_authorized, except: %i[index meme]
   respond_to :html
 
   def index
+  end
+
+  def meme
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,10 @@ Rails.application.routes.draw do
 
   root "classes#index"
 
+  resources :classes, only: [] do
+    collection do
+      get :meme
+    end
+  end
+
 end
